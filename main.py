10b7b8db2c002c19a2e08952d3fefa802c83a383
alexandermora1 <alexandermora@live.no>
2025-08-16 19:37:21 +0200

    Finished bookbot

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+import sys
 from stats import word_counter
 from stats import get_book_text
+from stats import character_counter
+from stats import sort_list
+
+def get_path():
+  if len(sys.argv) != 2:
+      print("Usage: python3 main.py <path_to_book>")
+      sys.exit(1)
+  else:
+      path = sys.argv[1]
+  return path
+
+def print_list(path, wordcount, dict_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {wordcount} total words")
+    print("--------- Character Count -------")
+    for item in dict_list:
+        print(f"{item['char']}: {item['num']}")
 
 def main():
-    #print(get_book_text("books/frankenstein.txt"))
-    return get_book_text("books/frankenstein.txt")
+    path = get_path()
+    text = get_book_text(path)
+    word_count = word_counter(text)
+    characters = character_counter(text)
+    dict_list = sort_list(characters)
+    print_list(path, word_count, dict_list)
 
-word_counter()
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,11 @@
+
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
     return file_contents
 
-
-def word_counter():
-    book_text = get_book_text("books/frankenstein.txt")
-    split_text = book_text.split()
+def word_counter(text):
+    split_text = text.split()
 
     counter = 0
     for word in split_text:
@@ -14,3 +13,26 @@ def word_counter():
 
     print(f"{counter} words found in the document")
     return counter
+
+def character_counter(text):
+    lower_case = text.lower()
+
+    count_chars = {}
+
+    for char in lower_case:
+        if char in count_chars:
+            count_chars[char] += 1
+        else:
+            count_chars[char] = 1
+    return count_chars
+    
+def sort_list(characters):
+    new_dict_list = [
+        {"char": key, "num": value}
+        for key, value in characters.items()
+    ]
+
+    new_dict_list.sort(reverse=True, key=lambda x: x["num"])
+
+    return new_dict_list
+        
